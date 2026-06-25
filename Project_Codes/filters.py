9f66a36a572073879b6de124aab2f772f5b1bd27
saikeshavramchinta-cmyk <saikeshavramchinta@gmail.com>
2026-06-25 18:00:51 +0530

    Create filters.py

---
+++ Project_Codes/filters.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def apply_volume_filter(df, period=20):
+    """
+    Checks if today's volume is higher than the 20-day average volume.
+    Returns a column of True/False values.
+    """
+    df['Volume_SMA_20'] = df['Volume'].rolling(window=period).mean()
+    df['Volume_Filter_Pass'] = df['Volume'] > df['Volume_SMA_20']
+    return df
