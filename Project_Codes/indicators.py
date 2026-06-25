968f906a1825d82aeb070bbfa9d9f7ed033ac2f7
saikeshavramchinta-cmyk <saikeshavramchinta@gmail.com>
2026-06-25 21:48:08 +0530

    Update indicators.py

--- Project_Codes/indicators.py
+++ Project_Codes/indicators.py
@@ -1,6 +1,6 @@
 import pandas as pd
 def calculate_macd(df):
-    """Calculates the standard MACD and Signal lines."""
+    # Calculates the standard MACD and Signal lines.
     df['EMA_12'] = df['Close'].ewm(span=12, adjust=False).mean()
     df['EMA_26'] = df['Close'].ewm(span=26, adjust=False).mean()
     df['MACD_Line'] = df['EMA_12'] - df['EMA_26']
@@ -8,7 +8,7 @@ def calculate_macd(df):
     return df
 
 def calculate_rsi(df, period=14):
-    """Calculates the standard 14-period RSI."""
+    # Calculates the standard 14-period RSI.
     df['Price_Change'] = df['Close'].diff()
     df['Gain'] = df['Price_Change'].where(df['Price_Change'] > 0, 0)
     df['Loss'] = -df['Price_Change'].where(df['Price_Change'] < 0, 0)
@@ -21,7 +21,7 @@ def calculate_rsi(df, period=14):
     return df
 
 def calculate_bollinger_bands(df, period=20, num_std=2):
-    """Calculates the Middle, Upper, and Lower Bollinger Bands."""
+    # Calculates the Middle, Upper, and Lower Bollinger Bands.
     df['BB_Middle'] = df['Close'].rolling(window=period).mean()
     rolling_std = df['Close'].rolling(window=period).std()
     
