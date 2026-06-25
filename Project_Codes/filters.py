fa749a50b34972a66d69bf573a6260b1842487b6
saikeshavramchinta-cmyk <saikeshavramchinta@gmail.com>
2026-06-25 22:13:11 +0530

    Update filters.py

--- Project_Codes/filters.py
+++ Project_Codes/filters.py
@@ -8,3 +8,4 @@ def apply_volume_filter(df, period=20):
     df['Volume_SMA_20'] = df['Volume'].rolling(window=period).mean()
     df['Volume_Filter_Pass'] = df['Volume'] > df['Volume_SMA_20']
     return df
+    
